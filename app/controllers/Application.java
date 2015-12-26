e3e6fa383ddbdbd662451a3163a5b2bb86c14ade
Kentaro Takakuwa <kenkuwa8276@gmail.com>
2015-12-26 18:27:42 +0900

    first commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Application extends Controller {
+
+    public Result index() {
+        Date now = new Date();
+
+        ArrayList<String> strs = new ArrayList<>(Arrays.asList("hoge", "poyo", "nyan"));
+
+        String date = now.toString();
+
+        return ok(index.apply(date, strs));
+    }
+
+}
