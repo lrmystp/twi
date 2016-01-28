a34a45c0113c2bac8b7b84f4a03e010135b207bb
Kentaro Takakuwa <kenkuwa8276@gmail.com>
2016-01-28 18:31:17 +0900

    display tweets of following users

--- app/service/TweetService.java
+++ app/service/TweetService.java
@@ -1,12 +1,21 @@
 package service;
 
+import com.avaje.ebean.Expr;
+import com.avaje.ebean.Model.Finder;
 import models.Tweet;
 import models.User;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class TweetService {
     final private UserService userService = new UserService();
+    final private static Finder<Long, Tweet> tweetFinder = new Finder<>(Tweet.class);
 
-    public void tweet(Long userId, String content) {
+    final private static int TIMELINE_TWEETS = 20;
+
+    public void tweet(long userId, String content) {
         final User author = userService.getUserById(userId);
 
         Tweet tweet = new Tweet();
@@ -14,4 +23,23 @@ public class TweetService {
         tweet.content = content;
         tweet.save();
     }
+
+    public List<Tweet> getTimeline(long userId) {
+        final User user = userService.getUserById(userId);
+
+        final Set<Long> followingIds = user.following.stream()
+                .map(u -> u.followee.userId)
+                .collect(Collectors.toSet());
+
+        final List<Tweet> tweets = tweetFinder.where()
+                .disjunction()
+                    .add(Expr.eq("author.userId", userId))
+                    .add(Expr.in("author.userId", followingIds))
+                .setMaxRows(TIMELINE_TWEETS)
+                .orderBy("createdAt DESC")
+                .findList();
+
+        return tweets;
+    }
+
 }
